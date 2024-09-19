1885ad6f6626ed6e0c0907437c480e2b9547042a
Seongjun Im <80080990+choral7451@users.noreply.github.com>
2024-09-20 01:08:52 +0900

    dev: client component

---
+++ src/app/products/layout.tsx
@@ -0,0 +1,25 @@
+import styles from './layout.module.css'
+import Link from "next/link";
+import type {Metadata} from "next";
+
+
+export const metadata: Metadata = {
+  title: "멋진 제품 사이트 | 전체 제품 확인",
+  description: "멋진 제품을 확인해 보세요",
+  icons: {
+    icon: '/favicon.ico',
+  }
+};
+
+
+export default function ProductsLayout({ children }: { children: React.ReactNode }) {
+  return <>
+    <nav className={styles.nav}>
+      <Link href='/products/women'>여성옷</Link>
+      <Link href='/products/man'>남성옷</Link>
+    </nav>
+    <section className={styles.product}>
+      {children}
+    </section>
+  </>
+}
