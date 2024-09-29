996a5cfbdd52f3b8226f8caca4a62122a4c35006
Seongjun Im <80080990+choral7451@users.noreply.github.com>
2024-09-30 01:12:29 +0900

    나머지 파일 추가

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import styles from "./layout.module.css"
+import Link from "next/link";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "멋진 제품 사이트",
+  description: "멋진 제품을 판매하는 곳입니다.",
+  icons: {
+    icon: '/favicon.ico',
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <header className={styles.header}>
+          <h1>Demo Note</h1>
+          <nav className={styles.nav}>
+            <Link href='/products'>Products</Link>
+            <Link href='/about'>About</Link>
+            <Link href='/contact'>Contact</Link>
+          </nav>
+        </header>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Counter from "@/components/Counter";
+
+export default function Home() {
+  return (
+    <>
+      <h1>홈페이지다!!</h1>
+      <Counter />
+    </>
+  )
+}
